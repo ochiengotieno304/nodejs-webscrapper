6e47dcef6d097fca8fd1da6cd29211c39751a690
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-01-18 11:48:17 +0300

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const feedDisplay = document.querySelector('#feed')
 
-fetch('http://localhost:8000/results')
+fetch('http://localhost:8000')
     .then(response => {return response.json()})
     .then(data => {
         data.forEach(article => {
